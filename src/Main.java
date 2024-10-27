2ea87d50646b0a07484379f59dbe35dc71c782e6
Zephyros <zxphyros@gmail.com>
2024-10-27 19:56:45 -0400

    heah

--- src/Main.java
+++ src/Main.java
@@ -59,6 +59,7 @@ public class Main{
         Timer timer = new Timer(); //new timer instance
         Snake.updateMovement(); //inits snake at positiion of 1
 
+        Board.Cell.createFood(); //initializes food item
         //method that gets called every (milliseconds defined in FPS variable) makes the snake move and shit
             TimerTask snakeMovement = new TimerTask() {
                 @Override
@@ -69,6 +70,7 @@ public class Main{
                     }else{
                         display.setText("GAME OVER!");
                         display.setBounds(WINDOW_SIZE/2, WINDOW_SIZE/2, WINDOW_SIZE, WINDOW_SIZE);
+                        //System.exit(0);
                     }
                     //TOOD: make a better solution for stopping the game
                 }
@@ -80,7 +82,7 @@ public class Main{
 
     protected static class Board extends Main{
         //init var
-        final static protected int BOARD_SIZE = 10;
+        final static protected int BOARD_SIZE = 20;
         //protected ArrayList<Cell> allCells = new ArrayList<>(); //arraylist of all the cells
         protected static Map<Integer, Cell> cellsByPosition = new HashMap<>(); //contains all the cells and their position so u can get a specific cell by finding them in the map via positional value
         protected static ArrayList<Cell> snakeCells = new ArrayList<>(); //has all the cellsssss that are part of the snake in them
@@ -107,13 +109,10 @@ public class Main{
                 if (position % BOARD_SIZE == 0) {
                     toDisplay.append("<br>"); //adds new row
                 }
-
-                //delete youngest cell
-
             }
             toDisplay.append("</html>"); //NGL I HAF NP IDEA HTML WAS POSSIBLE IN SWING AND I ONLY KNOW IT BECAUSE I HAD TO ADD LINEBREAKS TO THIS JLABEL
 
-            display.setText(String.valueOf(toDisplay)); //sets display text to
+            display.setText(String.valueOf(toDisplay)); //sets display text to the drawn board
             panel.repaint();
             panel.revalidate();
         }
@@ -131,10 +130,11 @@ public class Main{
                 Cell currentCell = snakeCells.get(i);
                 //depreciates age by 1
                 currentCell.age--;
+                System.out.println(currentCell.age);
 
                 //if 0, turn back into a regular board cell
                 if(currentCell.age<=0){
-                    currentCell.type = false;
+                    currentCell.type = "tile";
                     currentCell.changeAppearance(false); //sets appearance to regular ass cell LOL
                     snakeCells.remove(i);
                 }
@@ -142,18 +142,18 @@ public class Main{
         }
 
         //class manages attributes for individual cells
-        protected static final class Cell {
+        protected static  class Cell {
             boolean status = false; //if FALSE: cell is represented by an O | if TRUE: cell is an X
             char appearance; //actual display of the cell
             int POSITION; //this is the location data and is effectively rows+col. this is used because you have to run the values through a map to sort by an element and running 2 maps for rows and cols is lag-inducing
             int age; //used to determine which cell is cleared when the snake moves
-            boolean type; //true if snake cell, false if else
+            String type; //accepted params: TILE [regular floor tiles] | SNAKE [snake's body] | FOOD [grow the snake]
 
             //constructor method used for initialization: sets X/Y position
             private Cell(int position){
                 POSITION = position;
                 this.appearance = changeAppearance(false);
-                this.type = false;
+                this.type = "tile";
                 this.age = 0;
 
                 //allCells.add(this);
@@ -163,8 +163,8 @@ public class Main{
             //method used to change appearance of cell based on status
             private char changeAppearance(boolean status){ //true if snake false if no\t
                 //active/inactive vars may not be necessary but it's nice to have them easily configurable
-                final char ACTIVE = 'X'; //active/inactive appearances for each cell (active is if there's a snake/food on that tile)
-                final char INACTIVE = 'O';
+                final char ACTIVE = '■'; //active/inactive appearances for each cell (active is if there's a snake/food on that tile)
+                final char INACTIVE = '☐';
                 this.status = status;
 
                 //idk if they should be formatted like this but it looks nicer
@@ -174,16 +174,35 @@ public class Main{
             }
 
             //adds cells to snakeCell list
-            private void snakeCellsManagement(Cell targetCell){
-                if(targetCell.age!=0){
+            private void snakeCellsManagement(Cell targetCell) {
+                if (Objects.equals(targetCell.type, "snake")) {
                     System.out.println("BROTHER YOU ATE YOURSELF" + targetCell.age);
                     gameStatus = false;
 
+                } else if (Objects.equals(targetCell.type, "food")) {
+                    targetCell.type = "tile";
+                    targetCell.status = false;
+                    targetCell.changeAppearance(false);
+                    createFood();
+                } else {
+                    targetCell.type = "snake";
+                    targetCell.age = Snake.length+1; //set to one because the cells would immediately get depreciated to (length-1)
+                    snakeCells.add(this);
+                }
+            }
+
+            protected static void createFood(){
+                Cell targetCell = cellsByPosition.get(1); //inits to placeholder cell
+                Random rand = new Random(); //gets random class to call random cell pos
+
+                while(!Objects.equals(targetCell.type, "tile")){ //if selected cell is snake
+                    int position = rand.nextInt(CELL_COUNT)+1; //must be ++ because rolls start at 0
+                    targetCell = cellsByPosition.get(position); //gets atts of cell currently selected
                 }
 
-                targetCell.type = true;
-                targetCell.age = Snake.length;
-                snakeCells.add(this);
+                //changes type to food and changes appearance to activated char
+                targetCell.type = "food";
+                targetCell.changeAppearance(true);
             }
         }
     }
@@ -198,6 +217,12 @@ public class Main{
 
         public static void updateMovement(){
             position+=modifier;
+            Cell hoveredCell = cellsByPosition.get(position);
+            if(Objects.equals(hoveredCell.type, "food")){
+                length++;
+
+            }
+
             Board.updateCell(true, position, true); //snake turns the tile it's on into it's activated appearance
             drawBoard(panel,display); //updates board
         }
@@ -214,24 +239,15 @@ public class Main{
 
             }else if(key == KeyEvent.VK_UP){
                 direction = "RIGHT";
-                modifier = -10;
+                modifier = -BOARD_SIZE;
 
             }else if(key == KeyEvent.VK_DOWN){
                 direction = "RIGHT";
-                modifier = 10;
+                modifier = BOARD_SIZE;
 
             }
 
             updateMovement();
         }
-
-        /* ok so for the apple its like
-        its a subclass of cell
-        and it starts at a random pos when init and it has activated tex
-        and when the snake hits it then length++
-        like make some kinda att that the snake can tell its an apple
-        and then after that it changes to a new pos
-        make sure that it doesnt spawn inside the snake (use maps)
-         */
     }
 }

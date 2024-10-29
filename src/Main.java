0895b0f6d433b118402b10664a8069081ecfa345
Zephyros <zxphyros@gmail.com>
2024-10-29 16:45:16 -0400

    that stupid commit was a mistake

--- src/Main.java
+++ src/Main.java
@@ -5,12 +5,12 @@ import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
 public class Main {
-    protected static JLabel display = new JLabel(); //jlabel that displays the game
-    protected static JPanel panel = new JPanel(); //panel that the jlabel goes on
-    protected static boolean gameStatus = true; //is the game running
-    protected static int BOARD_SIZE = 20; //how many rowsXcol the board is
+    protected static JLabel display = new JLabel();
+    protected static JPanel panel = new JPanel();
+    protected static boolean gameStatus = true;
+    protected static int BOARD_SIZE = 20;
     static final int WINDOW_SIZE = (22 * BOARD_SIZE); //controls size of all panels and frames
-    private static final int CELL_COUNT = (int) Math.pow(BOARD_SIZE, 2); //total amount of cells
+    private static final int CELL_COUNT = (int) Math.pow(BOARD_SIZE, 2);
 
     static final JButton playAgain = new JButton("Play again"); //plays the game again
 
@@ -25,7 +25,7 @@ public class Main {
                     break;
                 }
             }
-        } catch (Exception e) {System.out.println("error with look and feel!\n------DETAILS------\n" + e.getMessage());}
+        }catch (Exception e){System.out.println("error with look and feel!\n------DETAILS------\n" + e.getMessage());}
 
 
         // create a window
@@ -57,12 +57,7 @@ public class Main {
 
     //manages game
     private static void runGame(JFrame frame) {
-        //init var
         final int[] pressedKey = new int[1]; //WHA THE ACTUAL FUCK IS INTELLIJ SMART SOLUTIONS MAKING MY CODE DO WHI IS THIS A FINAL INT ARRAY???
-        Board.createFood(); //initializes food item
-        final int FPS = 250; //how often the frame refreshes, in MILLISECONDS
-        Timer timer = new Timer(); //new timer instance
-
         //IDK WHAT THIS DOES INTELLIJ JUST ADDED IT
         frame.addKeyListener(new KeyAdapter() {
             public void keyPressed(KeyEvent e) {
@@ -71,15 +66,18 @@ public class Main {
             }
         });
 
+        final int FPS = 300; //how often the frame refreshes, in MILLISECONDS (this is 1000/4 instead of just 250 bc its easier to work with)
+        Timer timer = new Timer(); //new timer instance
+        Snake.updateMovement(); //inits snake at positiion of 1
 
-        //Snake.updateMovement(); //inits snake at positiion of 1
-
+        Board.Cell.createFood(); //initializes food item
         //method that gets called every (milliseconds defined in FPS variable) makes the snake move and shit
         TimerTask snakeMovement = new TimerTask() {
+            @Override
             public void run() {
 
-                if(gameStatus){Snake.changeDirection(pressedKey[0]);
-                }else{stopGame();}
+                if (gameStatus){Snake.changeDirection(pressedKey[0]);}
+                else{stopGame();}
             }
         };
 
@@ -93,96 +91,18 @@ public class Main {
         playAgain.setVisible(true);
     }
 
-    //holds data for snake
-    public static class Snake extends Board {
-        static int length = 1;
-        static String direction = "RIGHT";
-        static int position = 1; //thithe position of the cell the snake's head is in
-        static int modifier =1; //how mmany cells the snake will move by (aka: the direction)
-        private static int nextPos; //predicts next position via modifier
-
-
-        public static void updateMovement() {
-            if (nextPos>CELL_COUNT&&nextPos<= 0){stopGame(); //ERR HANDLER: if the next position would be out-of-bounds or otherwise invalid, stop the game
-
-            }else{
-                position += modifier; //makes the snake advance by however many tiles the direction needs them to advance in
-                Cell targetCell = cellsByPosition.get(position);  //this is the current cell that the snake's head is "hovering" over
-                nextPos = position + modifier;
-                checkBorders(targetCell); //calls snakeCellsManagement method to add the cell into the list of snake cells
-                cellAgeDeprecation();
-                drawBoard(panel, display);
-            }
-        }
-
-        //manages the cells that are a part of the snake, and other functions such as increasing length when food is eaten or causing a gameover when hitting the border or yourself
-        private static void checkBorders(Cell headCell) {
-            //init var
-            final int column = position % BOARD_SIZE; //gets the current column of the snake by dividing the position by the board size and getting the remainder
-            final int row = position / BOARD_SIZE; //gets the current row of the snake by dividing the position of the board size and truncating any decimal slots
-            final int nextRow = nextPos / BOARD_SIZE; //these nextRow/Col vars, CHANGE var, and the HORIZONTAL var are not necessary you can just use an entire statement for the if-statements but this is way more readable
-            final int nextCol = nextPos % BOARD_SIZE;
-            final boolean horizontal = direction.equals("LEFT") || direction.equals("RIGHT");
-            final boolean unauthChange = (horizontal &&nextRow!=row)&&(!horizontal &&nextCol!=column); //so how this works is that if there was no gameover condition when you hit a wall you would appear on the other side of the board on the next row/column so it's checking to see if you're appearing on a new row/col when your current direction logically wouldn't do that (so you had to have hit a border)
-
-            //System.out.println("----------------------------\nCURRENT ROW: "+row+" NEXT ROW: "+nextRow+"\nCURRENT COLUMN: "+column+" NEXT COLUMN: "+nextCol+"\nCURRENT POS: "+position+" NEXT POS:  "+nextPos+"\nDIRECTION: "+direction+" HORIZONTAL: "+HORIZONTAL+"\nMODIFIER: "+modifier+"\n----------------------------");
-            //^^^^^^^^ VERY LONG DEBUG MESSAGE ONLY ENABLE TO DEBUG POSITIONING AND GAMEOVER
-
-            //intellij here says that the unath_change var will always be false but it is a liar
-            //if you hit yourself or a border, quit the game and force-exit the current method
-            if(Objects.equals(headCell.type, "snake")|| unauthChange){stopGame();}
-
-            headCell.changeAppearance(true); //changes target cell into its activated appearance (since snake cells are the activated appearance of a shaded-in block
-            if (Objects.equals(headCell.type, "food")) { //this looks incredibly dumb but you have to have this if statement inside the else
-                length++;
-                Board.createFood();
-            }
-
-            headCell.type = "snake";
-            headCell.age = Snake.length + 1; //add one because the cells would immediately get depreciated to (length-1)
-            snakeCells.add(headCell);
-
-            }
-
-
-
-        private static void changeDirection(int key){
-
-            if(gameStatus) {
-                //me when code stolen off stackoverflow
-                //this feels like it could be improved tbh but idk what
-                if (key==KeyEvent.VK_RIGHT&&!Objects.equals(direction, "LEFT")) {
-                    direction = "RIGHT";
-                    modifier = 1;
-                } else if (key==KeyEvent.VK_LEFT&&!Objects.equals(direction, "RIGHT")) {
-                    direction = "LEFT";
-                    modifier = -1;
-
-                } else if (key==KeyEvent.VK_UP&&!Objects.equals(direction, "DOWN")) {
-                    direction = "UP";
-                    modifier = -BOARD_SIZE;
-                } else if (key==KeyEvent.VK_DOWN&&!Objects.equals(direction, "UP")) {
-                    direction = "DOWN";
-                    modifier = BOARD_SIZE;
-                }
-
-                updateMovement();
-
-            }else{stopGame();}
-
-        }
-    }
-
-
-    //class manages, holds, and initializes data for the entire board
     protected static class Board extends Main {
         //init var
+        //protected ArrayList<Cell> allCells = new ArrayList<>(); //arraylist of all the cells
         protected static Map<Integer, Cell> cellsByPosition = new HashMap<>(); //contains all the cells and their position so u can get a specific cell by finding them in the map via positional value
         protected static ArrayList<Cell> snakeCells = new ArrayList<>(); //has all the cellsssss that are part of the snake in them
 
         //constructor that initializes all cell values into the board
         protected Board() {
-            for (int position = 1; position <= CELL_COUNT; position++) {new Cell(position);} //creates a cell object for each position and age of 0
+            for (int position = 1; position <= CELL_COUNT; position++) {
+                new Cell(position);
+            } //creates a cell object for each position and age of 0
+
             drawBoard(panel, display);
         }
 
@@ -197,10 +117,10 @@ public class Main {
                 String input = " " + targetCell.appearance + " ";
 
                 toDisplay.append(input);
-                if (position % BOARD_SIZE == 0) {toDisplay.append("<br>");} //adds new row
-
+                if (position % BOARD_SIZE == 0) {
+                    toDisplay.append("<br>"); //adds new row
+                }
             }
-
             toDisplay.append("</html>");
 
             display.setText(String.valueOf(toDisplay)); //sets display text to the drawn board
@@ -208,7 +128,15 @@ public class Main {
             panel.revalidate();
         }
 
+        //updates a specific cell based on specified status and position
+        //* intellij is alwayas telling me that these vars are always set to snake/true when called and while that is correct it's better flexibility to keep these as vars
+        protected static void updateCell(boolean status, int position, String type) {
+            Cell targetCell = cellsByPosition.get(position);
+            targetCell.changeAppearance(status);
+        }
+
         //decreases age of all cells by 1 and removes any cells with an age of zero
+
         protected static void cellAgeDeprecation() {
             for (int i = 0; i < snakeCells.size(); i++) {
                 Cell currentCell = snakeCells.get(i);
@@ -222,20 +150,6 @@ public class Main {
                     snakeCells.remove(i);
                 }
             }
-        }
-
-        protected static void createFood() {
-            Random rand = new Random(); //gets random class to call random cell pos
-            Cell targetCell = cellsByPosition.get(rand.nextInt(CELL_COUNT) + 1); //inits to placeholder cell
-
-            while (!Objects.equals(targetCell.type, "tile")) { //if selected cell is snake
-                int RNGPos = rand.nextInt(CELL_COUNT) + 1; //must be ++ because rolls start at 0
-                targetCell = cellsByPosition.get(RNGPos); //gets atts of cell currently selected
-            }
-
-            //changes type to food and changes appearance to activated char
-            targetCell.type = "food";
-            targetCell.changeAppearance(true);
         }
 
         //class manages attributes for individual cells
@@ -272,7 +186,7 @@ public class Main {
             }
 
             //adds cells to snakeCell list
-            /*private void snakeCellsManagement(Cell targetCell) {
+            private void snakeCellsManagement(Cell targetCell) {
                 targetCell.changeAppearance(true); //changes target cell into its activated appearance (since snake cells are the activated appearance of a shaded-in block
                 if (Objects.equals(targetCell.type, "snake")) {stopGame();} //ends game if you run into yourself
                 else {
@@ -285,7 +199,86 @@ public class Main {
                     targetCell.age = Snake.length + 1; //add one because the cells would immediately get depreciated to (length-1)
                     snakeCells.add(this);
                 }
-            }*/
+            }
+
+            protected static void createFood() {
+                Random rand = new Random(); //gets random class to call random cell pos
+                Cell targetCell = cellsByPosition.get(rand.nextInt(CELL_COUNT) + 1); //inits to placeholder cell
+
+                while (!Objects.equals(targetCell.type, "tile")) { //if selected cell is snake
+                    int position = rand.nextInt(CELL_COUNT) + 1; //must be ++ because rolls start at 0
+                    targetCell = cellsByPosition.get(position); //gets atts of cell currently selected
+                }
+
+                //changes type to food and changes appearance to activated char
+                targetCell.type = "food";
+                targetCell.changeAppearance(true);
+            }
+        }
+    }
+
+    //holds data for snake
+    public static class Snake extends Board {
+        static int length = 1;
+        static String direction = "RIGHT";
+        static int position = 1; //thithe position of the cell the snake's head is in
+        static int modifier = 1; //how mmany cells the snake will move by (aka: the direction)
+        private static int nextPos; //predicts next position via modifier
+
+
+        public static void updateMovement() {
+            if (nextPos>CELL_COUNT&&nextPos<= 0) {stopGame(); //ERR HANDLER: if the next position would be out-of-bounds or otherwise invalid, stop the game
+
+            }else{
+                Cell targetCell = cellsByPosition.get(position);
+                position += modifier; //makes the snake advance by however many tiles the direction needs them to advance in
+                nextPos = position + modifier;
+                checkBorders(); //checks if snake is hitting an edge cell (this must be done AFTER the next cell is ran through validity checks because if it isnt then the snake will advance to the invalid cell before borderchecks are run and crash the game
+                targetCell.snakeCellsManagement(targetCell); //calls snakeCellsManagement method to add the cell into the list of snake cells
+                cellAgeDeprecation();
+                drawBoard(panel, display);
+            }
+        }
+
+        //checks to see if player ran into a wall
+        private static void checkBorders() {
+            final boolean HORIZONTAL = direction.equals("LEFT") || direction.equals("RIGHT");
+            final int column = position % BOARD_SIZE; //gets the current column of the snake by dividing the position by the board size and getting the remainder
+            final int row = position / BOARD_SIZE; //gets the current row of the snake by dividing the position of the board size and truncating any decimal slots
+            final int nextRow = nextPos / BOARD_SIZE; //these nextRow/Col vars are not neccessary you can just use an entire statement for the if-statements but this is more readable
+            final int nextCol = nextPos % BOARD_SIZE;
+
+            //System.out.println("----------------------------\nCURRENT ROW: "+row+" NEXT ROW: "+nextRow+"\nCURRENT COLUMN: "+column+" NEXT COLUMN: "+nextCol+"\nCURRENT POS: "+position+" NEXT POS:  "+nextPos+"\nDIRECTION: "+direction+" HORIZONTAL: "+HORIZONTAL+"\nMODIFIER: "+modifier+"\n----------------------------");
+            //VERY LONG DEBUG STRING DO NOT ENABLE UNLESS TESTING POSITIONING OR GAMEOVER CONDIITONALS
+
+            if(HORIZONTAL&&nextRow!=row){stopGame();
+
+            }else if(!HORIZONTAL&&nextCol!=column){stopGame();}
+        }
+
+
+
+        private static void changeDirection(int key){
+
+            if(gameStatus) {
+                //me when code stolen off stackoverflow
+                if (key==KeyEvent.VK_RIGHT&&!Objects.equals(direction, "LEFT")) {
+                    direction = "RIGHT";
+                    modifier = 1;
+                } else if (key==KeyEvent.VK_LEFT&&!Objects.equals(direction, "RIGHT")) {
+                    direction = "LEFT";
+                    modifier = -1;
+
+                } else if (key==KeyEvent.VK_UP&&!Objects.equals(direction, "DOWN")) {
+                    direction = "UP";
+                    modifier = -BOARD_SIZE;
+                } else if (key==KeyEvent.VK_DOWN&&!Objects.equals(direction, "UP")) {
+                    direction = "DOWN";
+                    modifier = BOARD_SIZE;
+                }
+
+                updateMovement();
+            }else{stopGame();}
         }
     }
 }
